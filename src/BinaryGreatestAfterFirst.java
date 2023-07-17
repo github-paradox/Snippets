af6b87ebe5ae00b005e75b5f57d0e3a8b0213102
github-paradox <121964349+github-paradox@users.noreply.github.com>
2023-07-17 23:40:11 +0530

    Binary Search Greatest After First Occurence

--- src/BinaryGreatestAfterFirst.java
+++ src/BinaryGreatestAfterFirst.java
@@ -1,9 +1,24 @@
 //To find the greatest no before the first occurence of the target element
+import java.util.*;
 public class BinaryGreatestAfterFirst 
 {
-   public static void main(String[] args) {
-    int arr[]=new int[]{0,1,1,1,1,2,3,4,5,5,5,5,6};
-    int mid,low=0,high=arr.length-1,target=7,ans=-1;
+   public static void main(String[] args) 
+   {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter the Number of elements");
+      int n=sc.nextInt();
+
+       int arr[]=new int[n];
+      
+      System.out.println("Enter the elements");
+      for(int i=0;i<n;i++)
+         arr[i]=sc.nextInt();
+
+      System.out.println("Enter the Target element to be found");
+      int target=sc.nextInt();
+      
+    int mid,low=0,high=arr.length-1,ans=-1;
+      
     while(low<=high)
     {
         mid=low+(high-low)/2;
@@ -17,9 +32,9 @@ public class BinaryGreatestAfterFirst
         else
         low=mid+1;
     }
-    if(ans>0 || ans!=-1)
-    System.out.println(arr[ans-1]);
+    if(ans>-1)
+    System.out.println("The Greatest Element found after "+target+" is the element "+arr[ans-1]);
     else
-    System.out.println("nothing");
+    System.out.println("No such element found");
    } 
 }
